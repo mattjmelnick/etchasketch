fe44011a8f2f71685e2acc007f709617311567b5
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2023-10-26 19:53:07 -0400

    Remove separate function to get number of squares, add to click button function which resolves bug that clears the window after repeating the prompt, add comments to explain longer function

--- etchasketch.js
+++ etchasketch.js
@@ -2,23 +2,23 @@ let gridContainer = document.querySelector(".grid-container");
 let changeSquaresButton = document.querySelector(".change-squares-button");
 changeSquaresButton.addEventListener("click", clickButton);
 
-function getNumberOfSquares() {
+function clickButton() {
+    // Prompt for number of squares per side
     let numberOfSquares = prompt("Enter a number between 1-100:");
     if (numberOfSquares < 1 || numberOfSquares > 100 || isNaN(Number(numberOfSquares))) {
-        getNumberOfSquares();
+        clickButton();
     }
     else {
-        return numberOfSquares;
-    }
-}
-
-function clickButton() {
-    let number = Number(getNumberOfSquares());
+    // Calculate the number of squares to add to the grid
+    let number = Number(numberOfSquares);
     let totalNumber = number * number;
-    let gridSquareHeightWidth = (644 / number - 2);
+    // Subtract 2 to account for the border around the square
+    let gridSquareHeightWidth = (650 / number - 2);
+    // Reset the grid after selecting a new number
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
+    // Create squares based on selected number
     for (let i = 0; i < totalNumber; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.toggle("grid-square");
@@ -28,8 +28,10 @@ function clickButton() {
     gridSquare.style.width = `${gridSquareHeightWidth}px`;
     gridContainer.appendChild(gridSquare);
     }
+    // Iterate through the squares and add the hover effect to change the color
     let grid = Array.from(document.querySelectorAll(".grid-square"));
     for (const square of grid) {
     square.addEventListener("mouseover", function() {square.style.backgroundColor = "black"})
+    }
     }
 }
